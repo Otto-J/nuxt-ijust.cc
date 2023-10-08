26b8678e3eeeb884b22ccea48884b220c1df289a
Otto <fa@ijust.cc>
2023-10-08 13:17:55 +0800

    ci: remove space, add sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
   // Fetch all documents
   const docs = await serverQueryContent(event).find();
   const sitemap = new SitemapStream({
-    hostname: "/",
+    hostname: "https://ijust.cc",
   });
 
   for (const doc of docs) {
